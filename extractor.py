aacebf9efeb27f7a6c7ef80d17c91716fd4b8730
n-d-r <crtl.ltrc.privat@googlemail.com>
2015-12-23 18:11:01 +0100

    transferred functionality from article class to extractor

--- extractor.py
+++ extractor.py
@@ -21,12 +21,35 @@ class KeywordExtractor(ParallelProcessor):
   def __init__(self):
     super(KeywordExtractor, self).__init__()
 
+  def _lf(keywords, text):
+    found_keywords = []
+    text_stripped = ''.join(
+      [char for char in text if char not in '\'.,()[]{}\"']
+    )
+    text_split = text_stripped.split()
+    for keyword in keywords:
+      if keyword in text_split:
+        found_keywords.append(keyword)
+    return list(set(found_keywords))
+
+  def _look_for(to_process_q, processed_q, **kwargs):
+    while not to_process_q.empty():
+      article_object = to_process_q.get()
+      article_object.keywords = self._lf(kwargs['keywords'], 
+                                         article_object.text)
+      processed_q.put(article_object)
+
   def look_for_keywords(self, keywords, to_process_q, processed_q):
     """
     Scans the text of the article objects in to_process_q for the keywords
     passed through the keywords argument.
     """
-    pass
+    self.execute(
+      function=self._look_for,
+      to_process_q=to_process_q,
+      processed_q=processed_q,
+      keywords=keywords
+    )
 
   def extract_keywords(self, to_process_q, processed_q):
     """
